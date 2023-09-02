7a48f2aff4de1e31a72f4f755fbe7ee89709e19c
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-09-02 23:53:26 +0530

    logo nabar form components etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,22 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/product' element={<Product />} />
-        <Route path='/pricing' element={<Pricing />} />
+        <Route index element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/app' element={<AppLayout />} />
+        <Route path='app' element={<AppLayout />}>
+          <Route index element={<CityList />} />
+          <Route path='cities' element={<CityList />} />
+          <Route path='countries' element={<p>List of countries</p>} />
+          <Route path='form' element={<p>Form</p>} />
+        </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Logo from "./Logo";
+
 import styles from "./Sidebar.module.css";
 
 function Sidebar() {
@@ -8,7 +10,7 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by Raviranjan Mahto
