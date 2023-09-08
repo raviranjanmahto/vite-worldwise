624124f4afd90648e9fb2348e5cf1ef10d74724e
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-09-09 00:52:11 +0530

    Protecting routes Auth Context

---
+++ client/src/pages/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { useEffect } from "react";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated) navigate("/");
+    },
+    [isAuthenticated, navigate]
+  );
+  return isAuthenticated ? children : null;
+};
+
+export default ProtectedRoute;
