3a62e726379a334952770f13eda4e1dfbeae6d99
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-09-08 19:48:44 +0530

    added city delete logic

--- client/src/contexts/CitiesContext.jsx
+++ client/src/contexts/CitiesContext.jsx
@@ -54,6 +54,20 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities(cities => cities.filter(city => city.id !== id));
+    } catch (error) {
+      alert("There was an error deleting city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
@@ -62,6 +76,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
